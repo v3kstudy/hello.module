49c8f04a44fa337118d7f92ea54f4f46b9c19ba4
Andy Truong <thehongtt@gmail.com>
2014-07-13 10:46:41 +0700

    Hi dynamic routes.

---
+++ src/Routing/DemoRoutesCallbacks.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\hello\Routing;
+
+use Symfony\Component\Routing\Route;
+
+class DemoRoutesCallbacks
+{
+
+    public function getRoutes()
+    {
+        $routes = [];
+
+        $routes['hello.dynamic_route'] = new Route(
+            '/hello_dynamic/{name}',
+            [
+                '_title' => 'Dynamic route demo',
+                '_content' => 'Drupal\\hello\\Controller\SimpleCallback::sayHiAction',
+                'name' => 'Dynamic Routing…',
+            ],
+            ['_permission' => 'access content']
+        );
+
+        return $routes;
+    }
+
+}
---
+++ src/Routing/RouteSubscriber.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\hello\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+class RouteSubscriber extends RouteSubscriberBase
+{
+
+    protected function alterRoutes(RouteCollection $collection)
+    {
+        if ($route = $collection->get('hello.sayHi')) {
+            $route->setDefault('name', 'Andy Truong');
+        }
+    }
+
+}
